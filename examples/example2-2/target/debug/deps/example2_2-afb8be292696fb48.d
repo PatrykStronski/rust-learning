cfb176703c7ceac6079a3b8c50d866ee5a79bc37
Patryk Stronski <p.stronski13@gmail.com>
2020-07-16 00:10:29 +0200

    another_example

--- examples/example2-2/target/debug/deps/example2_2-afb8be292696fb48.d
+++
@@ -1,5 +0,0 @@
-/home/azath/Documents/Projects/RustMentorship/examples/example2-2/target/debug/deps/example2_2-afb8be292696fb48: src/main.rs
-
-/home/azath/Documents/Projects/RustMentorship/examples/example2-2/target/debug/deps/example2_2-afb8be292696fb48.d: src/main.rs
-
-src/main.rs:
--- examples/example2-2/target/debug/example2-2.d
+++
@@ -1,1 +0,0 @@
-/home/azath/Documents/Projects/RustMentorship/examples/example2-2/target/debug/example2-2: /home/azath/Documents/Projects/RustMentorship/examples/example2-2/src/main.rs
