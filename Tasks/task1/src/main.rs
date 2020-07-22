a4b1d44ace72c84b0e7bdb14d2b9fd35f0ff4e0a
Paweł Pieniacki <ppieniacki@gmail.com>
2020-07-22 14:18:46 +0200

    task 1 completion proposition

--- Tasks/task1/src/main.rs
+++ Tasks/task1/src/main.rs
@@ -1,18 +1,18 @@
 fn main() {
     use std::collections::HashMap;
 
-    let text = "ust has a focus on safety and speed. It accomplishes these goals through many ‘zero-cost abstractions’, which means that in Rust, abstractions cost as little as possible in order to make them work. The ownership system is a prime example of a zero cost abstraction. All of the analysis we’ll talk about in this guide is done at compile time. You do not pay any run-time cost for any of these features";
+    let text = "Rust has a focus on safety and speed. It accomplishes these goals through many ‘zero-cost abstractions’, which means that in Rust, abstractions cost as little as possible in order to make them work. The ownership system is a prime example of a zero cost abstraction. All of the analysis we’ll talk about in this guide is done at compile time. You do not pay any run-time cost for any of these features";
     let mut letters_counter: HashMap<char, u8> = HashMap::new();
     
-    for char in text.chars() {
-        let mut curr_count: u8 = 0;
-        if letters_counter.contains_key(&char) {
-            curr_count = *letters_counter.get(&char).unwrap();
+    for single_char in text.chars().flat_map(char::to_lowercase).collect::<Vec<char>>() {
+        let curr_count: u8;
+        if letters_counter.contains_key(&single_char) {
+            curr_count = *letters_counter.get(&single_char).unwrap();
         } else {
             curr_count = 0;
         }
 
-        letters_counter.insert(char, curr_count + 1);
+        letters_counter.insert(single_char, curr_count + 1);
     }
 
     let mut chars: Vec<&char> = letters_counter.keys().collect();
