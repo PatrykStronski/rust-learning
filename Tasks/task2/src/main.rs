cafd44caea18a1b58a41e20432cd0925f8091b08
Paweł Pieniacki <ppieniacki@gmail.com>
2020-08-04 13:46:18 +0200

    task2 completion proposition

---
+++ Tasks/task2/src/main.rs
@@ -0,0 +1,58 @@
+struct Point {
+    x: f32,
+    y: f32,
+}
+
+impl Point {
+    fn distance_to(&self, p2: &Point) -> f32 {
+        ((self.x - p2.x).powi(2)+(self.y - p2.y).powi(2)).sqrt()
+    }
+
+    fn midpoint_with(&self, p2: &Point) -> Point {
+        Point {
+            x: (self.x + p2.x) / 2.,
+            y: (self.y + p2.y) / 2.,
+        }
+    }
+}
+
+struct Triangle {
+    points: [Point; 3]
+}
+
+impl Triangle {
+    fn get_area(&self) -> f32 {
+        let p1 = &self.points[0];
+        let p2 = &self.points[1];
+        let p3 = &self.points[2];
+        ((p1.x * (p2.y - p3.y) + p2.x * (p3.y - p1.y) + p3.x * (p1.y - p2.y)) / 2.).abs()
+    }
+}
+
+fn calculate_distance(p1: &Point, p2: &Point) -> f32 {
+    ((p1.x - p2.x).powi(2)+(p1.y - p2.y).powi(2)).sqrt()
+}
+
+fn main() {
+    let point1 = Point {
+        x: 1.524,
+        y: 9.284,
+    };
+
+    let point2 = Point {
+        x: 6.27,
+        y: 5.264,
+    };
+
+    let triangle = Triangle {
+        points: [Point { x: 11., y: 12. }, Point { x: 20., y: 20. }, Point { x: 30., y: 10. }]
+    };
+
+    let midpoint = point1.midpoint_with(&point2);
+
+    println!("Distance from function {}", calculate_distance(&point1, &point2));
+    println!("Distance from method {}", point1.distance_to(&point2));
+    println!("Midpoint from method [{}, {}]", midpoint.x, midpoint.y);
+    println!("Triangle area is {}", triangle.get_area());
+}
+
